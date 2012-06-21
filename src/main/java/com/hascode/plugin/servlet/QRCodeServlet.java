29561b50fbfa40f3aa795c22de27d0ef9f573065
hascode <hascode@localhost>
2012-06-21 22:21:34 +0200

    QR codes for projecs added.

--- src/main/java/com/hascode/plugin/servlet/QRCodeServlet.java
+++ src/main/java/com/hascode/plugin/servlet/QRCodeServlet.java
@@ -15,6 +15,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.atlassian.stash.nav.NavBuilder;
+import com.atlassian.stash.project.Project;
+import com.atlassian.stash.project.ProjectService;
 import com.atlassian.stash.repository.Repository;
 import com.atlassian.stash.repository.RepositoryService;
 import com.atlassian.stash.user.Permission;
@@ -24,6 +26,8 @@ import com.atlassian.stash.user.StashUser;
 import com.google.zxing.WriterException;
 import com.hascode.plugin.components.CodeCreator;
 
+import fj.data.Either;
+
 public class QRCodeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private final Logger log = LoggerFactory.getLogger(QRCodeServlet.class);
@@ -32,66 +36,126 @@ public class QRCodeServlet extends HttpServlet {
 	private final StashAuthenticationContext authContext;
 	private final RepositoryService repoService;
 	private final NavBuilder navBuilder;
+	private final ProjectService projectService;
 
 	public QRCodeServlet(final CodeCreator codeCreator,
 			final PermissionService permissionService,
 			final StashAuthenticationContext authContext,
-			final RepositoryService repoService, final NavBuilder navBuilder) {
+			final RepositoryService repoService, final NavBuilder navBuilder,
+			final ProjectService projectService) {
 		this.codeCreator = codeCreator;
 		this.permissionService = permissionService;
 		this.authContext = authContext;
 		this.repoService = repoService;
 		this.navBuilder = navBuilder;
+		this.projectService = projectService;
 	}
 
 	@Override
 	protected void doGet(final HttpServletRequest req,
 			final HttpServletResponse res) throws ServletException, IOException {
+		final String project = req.getParameter("p");
 		final String repository = req.getParameter("r");
-		if (StringUtils.isEmpty(repository)) {
-			log.warn("no repository given - we're out of here ...");
+
+		if (StringUtils.isEmpty(project) && StringUtils.isEmpty(repository)) {
+			log.warn("we need either a project or a repository here .. quitting");
 			signalError(res);
 			return;
+		}
+
+		final Either<Exception, BufferedImage> result = handleProjectOrRepository(
+				project, repository);
+		if (result.isLeft()) {
+			log.warn("no image created - reason is: "
+					+ result.left().value().getMessage(), result.left().value());
+			signalError(res);
+			return;
+		}
+
+		sendPngImage(res, result.right().value());
+	}
+
+	private Either<Exception, BufferedImage> handleProjectOrRepository(
+			final String project, final String repository) {
+		try {
+			final StashUser user = authContext.getCurrentUser();
+
+			// handle project
+			if (!StringUtils.isEmpty(project)) {
+				return handleProject(project, user);
+			}
+			// handle repository
+			else {
+				return handleRepository(repository, user);
+
+			}
+		} catch (Exception e) {
+			return Either.left(e);
+		}
+	}
+
+	private Either<Exception, BufferedImage> handleProject(
+			final String project, final StashUser user) throws Exception {
+		if (StringUtils.isEmpty(project)) {
+			throw new Exception("no project given");
+		}
+
+		final Project proj = projectService.getById(NumberUtils
+				.createInteger(project));
+		if (null == proj) {
+			throw new Exception("no project for given key: " + project);
+		}
+
+		if (null == user
+				|| !permissionService.hasProjectPermission(user, proj,
+						Permission.PROJECT_READ)) {
+			throw new Exception("user has no permission to view this project");
+		}
+
+		final String url = createUrl(proj);
+
+		final BufferedImage img = codeCreator.createURLCode(url);
+		if (img == null) {
+			throw new Exception("created image was null");
+		}
+		return Either.right(img);
+	}
+
+	private Either<Exception, BufferedImage> handleRepository(
+			final String repository, final StashUser user) throws Exception,
+			WriterException, IOException {
+		if (StringUtils.isEmpty(repository)) {
+			throw new Exception("no repository given");
 		}
 
 		final Repository repo = repoService.findRepositoryById(NumberUtils
 				.createInteger(repository));
 		if (null == repo) {
-			log.warn("no repository for given id '{}' found", repository);
-			signalError(res);
-			return;
+			throw new Exception("no repository for given id: " + repository);
 		}
 
-		final StashUser user = authContext.getCurrentUser();
 		if (null == user
 				|| !permissionService.hasRepositoryPermission(user, repo,
 						Permission.REPO_READ)) {
-			log.warn("user has no permission to view this repository");
-			signalError(res);
-			return;
+			throw new Exception(
+					"user has no permission to view this repository");
 		}
 
 		final String url = createUrl(repo);
 
-		try {
-			final BufferedImage img = codeCreator.createURLCode(url);
-			if (img == null) {
-				log.warn("created image was null");
-				signalError(res);
-				return;
-			}
-
-			sendPngImage(res, img);
-		} catch (WriterException e) {
-			log.warn(
-					"a writer exception occurred when trying to create the image",
-					e);
+		final BufferedImage img = codeCreator.createURLCode(url);
+		if (img == null) {
+			throw new Exception("created image was null");
 		}
-
+		return Either.right(img);
 	}
 
 	private String createUrl(final Repository repo) {
 		return navBuilder.repo(repo).buildAbsolute();
+	}
+
+	private String createUrl(final Project proj) {
+		return navBuilder.project(proj).buildAbsolute();
 	}
 
 	private void signalError(final HttpServletResponse res) {
